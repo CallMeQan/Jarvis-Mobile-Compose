e74035f4fd7a6beb073707ba9c60abb01c51a547
Đặng Ngọc Minh <104240011@student.vgu.edu.vn>
2025-06-19 15:05:39 +0700

    [Docs] Update ViewModel Scoping APIs and Communication documentation links
    Updated links in `SharedViewModel.kt` to point to the correct Android Developer documentation pages for ViewModel Scoping APIs and Fragment Communication.

--- app/src/main/java/com/github/callmeqan/jarviscomposed/utils/SharedViewModel.kt
+++ app/src/main/java/com/github/callmeqan/jarviscomposed/utils/SharedViewModel.kt
@@ -16,6 +16,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import android.content.Context
 
 class SharedViewModel() : ViewModel() {
 
